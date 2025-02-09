c68025bc874e8985f401877c2bcad2f4e709cace
KyonQi <kyon942755317@hotmail.com>
2025-02-09 22:43:55 +0100

    Finish basic LibOS

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,17 +1,25 @@
 #![no_std]
 #![no_main]
 mod lang_items;
+mod sbi;
+mod console;
 
 use core::arch::global_asm;
+
+use sbi::console_putchar;
 global_asm!(include_str!("entry.asm"));
 
 // SAFETY: there is no other global function of this name
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
     clear_bss();
-    loop {
+    console_putchar('o' as usize);
+    console_putchar('k' as usize);
+    println!("Hello, World");
+    panic!("Shutdown right now!");
+    // loop {
         
-    }
+    // }
 }
 
 // need to set 0 for .bss section
