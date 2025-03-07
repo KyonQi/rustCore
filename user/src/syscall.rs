dfe3353c3630b1ffb10a8126218110a5739be194
KyonQi <kyon942755317@hotmail.com>
2025-03-08 00:50:49 +0100

    Update user/syscall to support the new process requirement

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -6,6 +6,11 @@ const SYSCALL_YIELD: usize = 124;
 const SYSCALL_GET_TIME: usize = 169;
 const SYSCALL_SBRK: usize = 214;
 
+const SYSCALL_READ: usize = 63;
+const SYSCALL_GETPID: usize = 172;
+const SYSCALL_FORK: usize = 220;
+const SYSCALL_EXEC: usize = 221;
+const SYSCALL_WAITPID: usize = 260;
 
 #[inline(always)]
 fn sys_call(eid: usize, args: [usize; 3]) -> isize {
@@ -20,6 +25,10 @@ fn sys_call(eid: usize, args: [usize; 3]) -> isize {
         );
     }
     ret
+}
+
+pub fn sys_read(fd: usize, buffer: &mut [u8]) -> isize {
+    sys_call(SYSCALL_READ, [fd, buffer.as_mut_ptr() as usize, buffer.len()])
 }
 
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
@@ -40,4 +49,20 @@ pub fn sys_get_time() -> isize {
 
 pub fn sys_sbrk(size: i32) -> isize {
     sys_call(SYSCALL_SBRK, [size as usize, 0, 0])
+}
+
+pub fn sys_getpid() -> isize {
+    sys_call(SYSCALL_GETPID, [0, 0, 0])
+}
+
+pub fn sys_fork() -> isize {
+    sys_call(SYSCALL_FORK, [0, 0, 0])
+}
+
+pub fn sys_exec(path: &str) -> isize {
+    sys_call(SYSCALL_EXEC, [path.as_ptr() as usize, 0, 0])
+}
+
+pub fn sys_waitpid(pid:usize, exit_code: *mut i32) -> isize {
+    sys_call(SYSCALL_WAITPID, [pid as usize, exit_code as usize, 0])
 }
